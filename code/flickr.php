602c8b628b35c38d984f05542999ea288bd383a4
dave <mrdavelee2012@gmail.com>
2015-08-28 12:45:41 +0100

    First commit

---
+++ code/flickr.php
@@ -0,0 +1,21 @@
+    <?php
+    header('Content-type: application/json');
+    // Tags variable is used here to provide future use of search terms passed as arguments
+
+    $tags = 'chamonix,ski,snow';
+    //$data = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=' . $tags . '&format=json&jsoncallback=?';
+    $data = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=' . $tags . '&format=json';
+
+    // Flickr provides the option to import data as preformatted JSON. As this removes all possible risk of corruption this was used here.
+    //jsoncallback=? was used to remove the JavaScript wrapper around the JSON
+   
+    $flickrCi = curl_init($data);
+    curl_setopt($flickrCi,CURLOPT_RETURNTRANSFER, TRUE);
+    $flickrInput = curl_exec($flickrCi);
+    curl_close($flickrCi);
+
+    $flickrInput = str_replace('jsonFlickrFeed(','',$flickrInput);
+        $flickrInput = str_replace('})','}',$flickrInput);
+        echo $flickrInput;
+
+     ?>
